590e49427dc581754055123a53c6aca5a3691bab
G <>
2019-03-16 13:54:25 -0400

    url

--- server/index.js
+++ server/index.js
@@ -74,8 +74,4 @@ if (cluster.isMaster) {
   app.get('/:id', (req, res) => {
     res.sendFile(path.join(__dirname + '/../public/index.html'));
   });
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
 }
