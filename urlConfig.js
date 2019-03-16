bcebb43e4940a316cc1ecb00d1bafb412cfe0f28
G <>
2019-03-16 13:46:18 -0400

    for server3

--- urlConfig.js
+++ urlConfig.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: 'http://ec2-13-58-89-120.us-east-2.compute.amazonaws.com/api/products/'
+  url: 'http://ec2-18-216-242-162.us-east-2.compute.amazonaws.com/api/products/'
 };
